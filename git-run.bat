5f6d0c8eb533cbd7690b44062251773f4a4453ee
25206 <25206@EBI114TFWW.corp.hexaware.com>
2016-08-10 16:01:03 +0530

    Initial commit

---
+++ git-run.bat
@@ -0,0 +1,21 @@
+set path=D:\Softwares\Git\bin;%path%
+
+git --version
+
+cd  D:/Jeffer/WorkSpaces/runtime-EclipseApplication
+
+cd D:
+
+git init
+
+git add .
+
+git reset -- git.bat
+
+git commit -m "Initial commit"
+
+git remote rm origin
+
+git remote add origin http://://25206:as009318@http://172.25.164.110:9000/OEG-ADM/HexaJavaFactoryTesting
+
+git push origin master
